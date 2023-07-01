b78f9fef94e12eca81842e082704590a76cae91f
Rafl27 <raflsouza27@gmail.com>
2023-07-01 17:08:29 -0300

    logging added

--- src/main/kotlin/com/biteSized/bitesizedv4/controller/UserController.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/controller/UserController.kt
@@ -9,14 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/users")
 class UserController(@Autowired private val userRepository: UserRepository) {
 
+    private val logger: Logger = Logger.getLogger(UserController::class.java.name)
     @PostMapping
     fun createUser(@RequestBody newUser: User) : ResponseEntity<User> {
         val createdUser = userRepository.save(newUser)
+
+        // Logging the user creation
+        logger.info("New user created: ${createdUser.username}")
+
         return ResponseEntity(createdUser, HttpStatus.CREATED)
     }
 }
