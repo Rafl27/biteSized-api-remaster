0a863877e9a5cdd179106f1ba5b71f3a55212cc8
Rafl27 <raflsouza27@gmail.com>
2023-08-08 22:25:21 -0300

    getStoryAndUserNoComment query

--- src/main/kotlin/com/biteSized/bitesizedv4/repository/StoryRepository.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/StoryRepository.kt
@@ -1,5 +1,6 @@
 package com.biteSized.bitesizedv4.repository
 
+import com.biteSized.bitesizedv4.DTO.CompleteStoryNoComments
 import com.biteSized.bitesizedv4.model.Story
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -11,4 +12,9 @@ interface StoryRepository :JpaRepository<Story, Long> {
             "comment.downvotes as downvotesComment, comment.upvotes as upvotesCommment, comment.user_id as useridComment " +
             "FROM story JOIN comment ON story.id = comment.story_id WHERE story.id = :storyId ORDER BY comment.upvotes DESC", nativeQuery = true)
     fun getStoryComments(@Param("storyId") storyId: Long): List<Array<Any>>
+
+    @Query("SELECT profile_picture, username, art, date, downvotes, upvotes, title, content, story.id as storyId from bitesized.user\n" +
+            "join story on story.user_id = user.id;", nativeQuery = true)
+    fun getStoryAndUserNoComment() : List<Array<Any>>
+
 }
