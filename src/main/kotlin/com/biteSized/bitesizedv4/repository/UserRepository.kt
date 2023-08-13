d21bc6c2577d5e5d2722cc3d02fa67fe53e22d53
Rafl27 <raflsouza27@gmail.com>
2023-08-13 18:11:07 -0300

    getUserBasicInfoById query.

--- src/main/kotlin/com/biteSized/bitesizedv4/repository/UserRepository.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/UserRepository.kt
@@ -1,10 +1,15 @@
 package com.biteSized.bitesizedv4.repository
 
+import com.biteSized.bitesizedv4.DTO.UserBasicInfo
 import com.biteSized.bitesizedv4.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsernameAndPassword(username: String, password: String): User
     fun findByUsername(username: String): User
     fun findByEmail(email: String) : User
+    @Query("SELECT u.username, u.email, u.profilePicture FROM User u WHERE u.id = :userId")
+    fun getUserBasicInfoById(userId: Long): Array<String>
 }
--- src/main/kotlin/com/biteSized/bitesizedv4/service/UserService.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/service/UserService.kt
@@ -1,10 +1,12 @@
 package com.biteSized.bitesizedv4.service
 
 import com.biteSized.bitesizedv4.DTO.LoginRequest
+import com.biteSized.bitesizedv4.DTO.UserBasicInfo
 import com.biteSized.bitesizedv4.model.User
 import org.springframework.http.ResponseEntity
 
 interface UserService {
     fun createUser(newUser : User) : ResponseEntity<User>
-    fun login(loginRequest: LoginRequest) : ResponseEntity<String>
+    fun login(loginRequest : LoginRequest) : ResponseEntity<String>
+    fun userInfo(authorizationHeader : String) : ResponseEntity<UserBasicInfo>
 }
