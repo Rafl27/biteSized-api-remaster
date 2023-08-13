93ad47ab7ae3b539f357e181cd8c4e5519f25a7d
Rafl27 <raflsouza27@gmail.com>
2023-08-13 18:11:26 -0300

    /info controller

--- src/main/kotlin/com/biteSized/bitesizedv4/controller/UserController.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/controller/UserController.kt
@@ -1,18 +1,14 @@
 package com.biteSized.bitesizedv4.controller
 
 import com.biteSized.bitesizedv4.DTO.LoginRequest
+import com.biteSized.bitesizedv4.DTO.UserBasicInfo
 import com.biteSized.bitesizedv4.model.User
 import com.biteSized.bitesizedv4.repository.UserRepository
 import com.biteSized.bitesizedv4.security.JwtUtil
 import com.biteSized.bitesizedv4.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.security.crypto.bcrypt.BCrypt
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.*
-import java.util.logging.Logger
-import kotlin.random.Random
 
 
 @RestController
@@ -29,5 +25,10 @@ class UserController(@Autowired private val userRepository: UserRepository, @Aut
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<String> {
         return userService.login(loginRequest)
+    }
+
+    @GetMapping("/info")
+    fun userInfo(@RequestHeader("Authorization") authorizationHeader: String) : ResponseEntity<UserBasicInfo>{
+        return userService.userInfo(authorizationHeader)
     }
 }
