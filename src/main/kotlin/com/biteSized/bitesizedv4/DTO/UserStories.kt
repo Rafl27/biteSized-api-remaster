d4ec56bc91f25f87ce5f416611e367bdee63f117
Rafl27 <raflsouza27@gmail.com>
2023-08-13 18:09:55 -0300

    adding more field to userStories DTO

--- src/main/kotlin/com/biteSized/bitesizedv4/DTO/UserStories.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/DTO/UserStories.kt
@@ -3,5 +3,8 @@ package com.biteSized.bitesizedv4.DTO
 data class UserStories(
     val id: Long,
     val title: String,
-    val content: String
+    val content: String,
+    val upvotes: Int?,
+    val downvotes: Int?,
+    val art: String
 )
