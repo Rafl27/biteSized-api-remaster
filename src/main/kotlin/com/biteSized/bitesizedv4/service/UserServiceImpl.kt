d547a81c395bddaa0044a01f19a974fa7f76776b
Rafl27 <raflsouza27@gmail.com>
2023-08-14 21:29:36 -0300

    userInfoBasedOnStoryId service implementation

--- src/main/kotlin/com/biteSized/bitesizedv4/service/UserServiceImpl.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/service/UserServiceImpl.kt
@@ -4,8 +4,10 @@ import com.biteSized.bitesizedv4.DTO.LoginRequest
 import com.biteSized.bitesizedv4.DTO.UserBasicInfo
 import com.biteSized.bitesizedv4.controller.UserController
 import com.biteSized.bitesizedv4.model.User
+import com.biteSized.bitesizedv4.repository.StoryRepository
 import com.biteSized.bitesizedv4.repository.UserRepository
 import com.biteSized.bitesizedv4.security.JwtUtil
+import com.biteSized.bitesizedv4.util.StringArrayIntoDTO
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.bcrypt.BCrypt
@@ -17,7 +19,8 @@ import kotlin.random.Random
 class UserServiceImpl(
     private val userRepository: UserRepository,
     private val jwtUtil: JwtUtil,
-    private val tokenService: TokenService
+    private val tokenService: TokenService,
+    private val stringArrayIntoDTO: StringArrayIntoDTO
 ) : UserService {
     private val logger: Logger = Logger.getLogger(UserController::class.java.name)
     override fun createUser(newUser: User): ResponseEntity<User> {
@@ -79,4 +82,14 @@ class UserServiceImpl(
 
         return ResponseEntity.notFound().build()
     }
+
+    override fun userInfoBasedOnStoryId(storyId: Int): ResponseEntity<UserBasicInfo> {
+        val queryResult = userRepository.getUserBasicInfoByStoryId(storyId)
+
+        if (queryResult.isNotEmpty()) {
+            return ResponseEntity.ok(stringArrayIntoDTO.userBasicInfo(queryResult))
+        }
+        return ResponseEntity.notFound().build()
+    }
+
 }
