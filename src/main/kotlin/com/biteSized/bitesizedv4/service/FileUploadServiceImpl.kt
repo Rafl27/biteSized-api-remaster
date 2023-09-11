892aa17294bf10299bc61c2dd7da8b47a0f3bb55
Rafl27 <raflsouza27@gmail.com>
2023-09-10 22:12:08 -0300

    image compression.

--- src/main/kotlin/com/biteSized/bitesizedv4/service/FileUploadServiceImpl.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/service/FileUploadServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.biteSized.bitesizedv4.service
 
+import net.coobird.thumbnailator.Thumbnails
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -8,6 +9,7 @@ import java.nio.file.Path
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 import java.io.IOException
+import java.util.*
 
 @Service
 class FileUploadServiceImpl (@Value("\${file.upload.directory}") private val uploadDirectory: String) : FileUploadService {
@@ -21,8 +23,18 @@ class FileUploadServiceImpl (@Value("\${file.upload.directory}") private val upl
 
     override fun uploadFile(file: MultipartFile) {
         try {
-            val destinationPath = Path.of(uploadDirectory, file.originalFilename!!)
-            Files.copy(file.inputStream, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+            val allowedImageTypes = setOf("image/jpeg", "image/jpg", "image/png", "image/gif")
+            val contentType = file.contentType
+
+            if (contentType != null && allowedImageTypes.contains(contentType.lowercase(Locale.getDefault()))) {
+                val destinationPath = Path.of(uploadDirectory, file.originalFilename!!)
+
+                Thumbnails.of(file.inputStream)
+                    .scale(0.5)
+                    .toFile(destinationPath.toFile())
+            } else {
+                throw IllegalArgumentException("Invalid file type. Only image files (JPEG, PNG, GIF) are allowed.")
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
