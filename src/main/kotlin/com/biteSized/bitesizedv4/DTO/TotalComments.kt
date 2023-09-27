f60abaec4e87f4b9aed3581fb5088427d2ca0b06
Rafl27 <raflsouza27@gmail.com>
2023-09-26 22:02:04 -0300

    totalComments DTO.

---
+++ src/main/kotlin/com/biteSized/bitesizedv4/DTO/TotalComments.kt
@@ -0,0 +1,9 @@
+package com.biteSized.bitesizedv4.DTO
+
+import java.math.BigDecimal
+import java.math.BigInteger
+
+data class TotalComments(
+    val storyId: Long,
+    val totalComments: Long
+)
