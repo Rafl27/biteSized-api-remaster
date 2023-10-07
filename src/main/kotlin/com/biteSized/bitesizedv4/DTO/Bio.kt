495234601ba41bb9b4009d1468ef6e58bf60cfb4
Rafl27 <raflsouza27@gmail.com>
2023-10-07 11:06:30 -0300

    Bio DTOs

---
+++ src/main/kotlin/com/biteSized/bitesizedv4/DTO/Bio.kt
@@ -0,0 +1,5 @@
+package com.biteSized.bitesizedv4.DTO
+
+data class Bio(
+        val bio : String
+)
---
+++ src/main/kotlin/com/biteSized/bitesizedv4/DTO/UserBio.kt
@@ -0,0 +1,6 @@
+package com.biteSized.bitesizedv4.DTO
+
+data class UserBio(
+        val userId : Int,
+        val bio : String
+)
