e116be7cb5cc43ef24c34e1cefcefffa6769cffe
Rafl27 <raflsouza27@gmail.com>
2023-10-12 23:52:33 -0300

    add Id and try catch to userinfo service implementation.

--- src/main/kotlin/com/biteSized/bitesizedv4/service/UserService.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/service/UserService.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 interface UserService {
     fun createUser(newUser : User) : ResponseEntity<User>
     fun login(loginRequest : LoginRequest) : ResponseEntity<String>
-    fun userInfo(authorizationHeader : String) : ResponseEntity<UserBasicInfo>
+    fun userInfo(authorizationHeader : String) : ResponseEntity<Any>
     fun userInfoBasedOnStoryId(storyId: Int) : ResponseEntity<UserBasicInfo>
     fun userInfoBasedOnCommentId(userId: Int) : ResponseEntity<UserBasicInfo>
     fun createBio(userId : Int, bio: Bio, authorizationHeader : String) : ResponseEntity<UserBio>
