a03cfd27e49517e7924d583d57ee459c8fb2bf4c
Rafl27 <raflsouza27@gmail.com>
2023-11-18 23:13:46 -0300

    getAllUserVotes query.

---
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/CommentVoteRepository.kt
@@ -0,0 +1,14 @@
+package com.biteSized.bitesizedv4.repository
+
+import com.biteSized.bitesizedv4.model.CommentVote
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface CommentVoteRepository : JpaRepository<CommentVote, Long> {
+
+    @Query(" SELECT comment.id, art, content, date, downvotes, upvotes, story_id, vote_type" +
+            " FROM comment JOIN comment_vote ON comment_vote.user_id = comment.user_id" +
+            " WHERE comment.user_id = :userId", nativeQuery = true)
+    fun getAllUserVotes(@Param("userId") userId: Long) : List<Array<Any>>
+}
