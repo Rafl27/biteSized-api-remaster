ff5c5337c176d663f60f2e9296ecc4cf0cb1290d
Rafl27 <raflsouza27@gmail.com>
2023-11-20 13:52:47 -0300

    query fix.

--- src/main/kotlin/com/biteSized/bitesizedv4/repository/CommentVoteRepository.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/CommentVoteRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param
 interface CommentVoteRepository : JpaRepository<CommentVote, Long> {
 
     @Query(" SELECT comment.id, art, content, date, downvotes, upvotes, story_id, vote_type" +
-            " FROM comment JOIN comment_vote ON comment_vote.user_id = comment.user_id" +
-            " WHERE comment.user_id = :userId", nativeQuery = true)
+            " FROM comment JOIN comment_vote ON comment_vote.comment_id = comment.id" +
+            " WHERE comment_vote.user_id = :userId", nativeQuery = true)
     fun getAllUserVotes(@Param("userId") userId: Long) : List<Array<Any>>
 }
