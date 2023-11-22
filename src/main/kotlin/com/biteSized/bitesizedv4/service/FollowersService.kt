1008b2be1be7d8a45d47ec8a4b02bf31e7aa5ad5
Rafl27 <raflsouza27@gmail.com>
2023-11-22 12:23:11 -0300

    unfollow interface.

--- src/main/kotlin/com/biteSized/bitesizedv4/service/FollowersService.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/service/FollowersService.kt
@@ -1,9 +1,10 @@
 package com.biteSized.bitesizedv4.service
 
-import com.biteSized.bitesizedv4.DTO.FollowResponse
+import com.biteSized.bitesizedv4.DTO.UnfollowResponse
 import com.biteSized.bitesizedv4.model.Followers
 import org.springframework.http.ResponseEntity
 
 interface FollowersService {
     fun followUser(mainUserId : Long, follower : Long) : ResponseEntity<Followers>
+    fun unfollowUser(mainUserId : Long, follower : Long) : ResponseEntity<UnfollowResponse>
 }
