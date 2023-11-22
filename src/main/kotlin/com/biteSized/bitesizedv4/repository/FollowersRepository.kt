ca48271d250937cc231d0f3c4a3c309889cc9946
Rafl27 <raflsouza27@gmail.com>
2023-11-22 14:00:29 -0300

    findByMainUserAndFollower

--- src/main/kotlin/com/biteSized/bitesizedv4/repository/FollowersRepository.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/FollowersRepository.kt
@@ -2,6 +2,8 @@ package com.biteSized.bitesizedv4.repository
 
 import com.biteSized.bitesizedv4.model.Followers
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface FollowersRepository : JpaRepository<Followers, Long> {
+    fun findByMainUserAndFollower(mainUser: Long, follower: Long): Optional<Followers>
 }
