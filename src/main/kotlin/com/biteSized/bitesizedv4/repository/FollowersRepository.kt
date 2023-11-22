1d7d6a146876099f41d3ccae80cea09a72fd7ac7
Rafl27 <raflsouza27@gmail.com>
2023-11-22 14:48:02 -0300

    follower count query.

--- src/main/kotlin/com/biteSized/bitesizedv4/repository/FollowersRepository.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/repository/FollowersRepository.kt
@@ -1,9 +1,15 @@
 package com.biteSized.bitesizedv4.repository
 
+import com.biteSized.bitesizedv4.DTO.FollowerCount
 import com.biteSized.bitesizedv4.model.Followers
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.*
 
 interface FollowersRepository : JpaRepository<Followers, Long> {
     fun findByMainUserAndFollower(mainUser: Long, follower: Long): Optional<Followers>
+    fun findByMainUser(mainUser: Long) : Optional<Followers>
+    @Query("SELECT COUNT(*) from followers WHERE follower = :userId", nativeQuery = true)
+    fun getFollowerCount(@Param("userId") userId : Long) : FollowerCount
 }
