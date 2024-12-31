e23b6c224aee3fb3972aa52d768b08cfce4fcf1f
Rafl27 <raflsouza27@gmail.com>
2024-12-31 17:30:03 -0300

    set id to 0

--- src/main/kotlin/com/biteSized/bitesizedv4/model/UserTotalVotes.kt
+++ src/main/kotlin/com/biteSized/bitesizedv4/model/UserTotalVotes.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.*
 data class UserTotalVotes(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    val id: Long = 0,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
